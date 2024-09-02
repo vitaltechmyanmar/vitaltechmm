3037bd199756d109aaee79cd894f33a3b727d8b0
sc3p73r-it <sc3p73r@outlook.com>
2024-09-02 19:47:58 +0630

    contact us form change

--- forms/contact.php
+++ forms/contact.php
@@ -7,7 +7,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+  $receiving_email_address = 'htetoowaiyan@vitaltechmyanmar.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
@@ -29,7 +29,7 @@
      'host' => 'smtp.zoho.com',
      'username' => 'htetoowaiyan@vitaltechmyanmar.com',
      'password' => 'Dream@123!@#',
-     'port' => '587'
+     'port' => '465'
    );
   // */
 
